23425f57658bd4e254651359ebcf3a09f5eb3c5f
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-06 23:16:17 +0530

    Kubernetes

--- images/01-http-content/run.sh
+++ images/01-http-content/run.sh
@@ -1,7 +1,7 @@
 mkdir -p /usr/share/nginx/html
-#while true ; do
-#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
-#  sleep 1
-#done
-echo Hello World >/usr/share/nginx/html/index.html
+while true ; do
+  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
+  sleep 1
+done
+#echo Hello World >/usr/share/nginx/html/index.html
 
